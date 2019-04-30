bf7afce203541d6a64e4f0e325445d8b6d325337
Chris Zubak-Skees <chriszs@gmail.com>
2019-04-30 16:57:20 -0400

    Peg state count

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div class="basic-text">
         <!--<p class="chatter">The Center for Public Integrity reached out to all 50 states to ask if candidates running for state office may accept cryptocurrency. In total, {{responded.length-1}} states and D.C. responded.</p>-->
 
-        <h4>{{apnumber(passed.length)}} states enacted measures discouraging boycotts of Israel</h4>
+        <h4>27 states enacted measures discouraging boycotts of Israel</h4>
         <statebin :rows="passed" :labels="['Enacted']" :colors="['#f5e205']" />
     </div>
 </template>
@@ -25,6 +25,8 @@ export default {
             'https://docs.google.com/spreadsheets/d/e/2PACX-1vT5E5RES-wb2wxjVXcUkpstaJNWIkYUl9Mr703Ek8yd8OUIHHvaIekAyEg0SXPJIz6JPokNlDHFshNs/pub?gid=0&single=true&output=csv';
         let csv = await app.$axios.$get(spreadsheetUrl);
         let rows = await csvParse(csv);
+
+
 
         return {
             passed: rows.filter(row => row['passed?'].toLowerCase().trim() === 'yes')
